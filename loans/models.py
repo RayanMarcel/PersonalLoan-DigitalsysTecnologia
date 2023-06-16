04e9f5a30136824c37e2af29ce87b6216727a95d
rayanmpc <rayan@UbuntuServer.myguest.virtualbox.org>
2023-06-15 20:49:57 -0400

    implementada a model Proposal para comportar as informacoes do formulario

--- loans/models.py
+++ loans/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from .choices import STATUS_CHOICES
 
-# Create your models here.
+class Proposal(models.Model):
+    full_name = models.CharField(max_length=200)
+    cpf = models.CharField(max_length=14)
+    address = models.CharField(max_length=400)
+    loan_value = models.DecimalField(max_digits=15, decimal_places=2)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='PENDING')
+    reviewed = models.BooleanField(default=False)
